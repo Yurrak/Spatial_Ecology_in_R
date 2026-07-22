8ad2d6f385ca502a5d26a002d837e6ee59ab6948
Wei Gao <156875559+Yurrak@users.noreply.github.com>
2026-07-22 17:53:15 +0200

    Add files via upload

---
+++ code/01_first_script.R
@@ -0,0 +1,32 @@
+2 + 3
+
+
+kat <- 2 + 3
+kat
+
+deanna <- 5 + 7
+deanna
+
+
+kat + deanna
+
+
+kat * deanna
+
+ade <- c(10, 20, 50, 80, 95)
+lara <- c(3, 5, 15, 27, 30)
+
+
+
+plot(lara, ade)
+
+
+plot(lara, ade, xlab="all taxa species richness", ylab="insects species richness")
+
+plot(lara, ade, xlab="all taxa species richness", ylab="insects species richness", col="blue")
+
+plot(lara, ade, xlab="all taxa species richness", ylab="insects species richness", col="blue", pch=19)
+
+plot(lara, ade, xlab="all taxa species richness", ylab="insects species richness", col="blue", pch=19, cex=2)
+
+plot(lara, ade, xlab="all taxa species richness", ylab="insects species richness", col="blue", pch=19, cex=.5)
